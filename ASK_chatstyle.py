4e9f4fd10483a55e263619ee980f2760487a3bd3
Drew W <20048187+dvvilkins@users.noreply.github.com>
2023-10-16 21:41:56 -0400

    Update ASK_chatstyle.py

--- ASK_chatstyle.py
+++ ASK_chatstyle.py
@@ -46,7 +46,7 @@ st.image("https://github.com/dvvilkins/ASK/blob/4eb768800da3e592582f08b980fcc5e5
 # st.title("ASK Auxiliary Source of Knowledge")
 st.write(
     "#### ASK finds answers to USCG Auxiliary questions based on authoritative sources.")
-st.write("ASK uses Artificial Intelligence (AI) to search over 300 Coast Guard Auxiliary documents &mdash; AUXMAN, USCG Directives, BSX Policy Letters, SOPs, ALAUXs, etc. &mdash; to answer your questions. Click [here](https://drive.google.com/file/d/1BWY5zEg8yKIrS3Tx13VBpSlpPx7rKl25/view?usp=sharing) for the full list.", unsafe_allow_html=True)
+st.write("ASK uses Artificial Intelligence (AI) to search over 300 Coast Guard Auxiliary references to answer your questions. Click [here](https://drive.google.com/file/d/1BWY5zEg8yKIrS3Tx13VBpSlpPx7rKl25/view?usp=sharing) for the full list.", unsafe_allow_html=True)
 st.write("  ")
 st.write("""  
     **ASK helps answers questions such as:**   
