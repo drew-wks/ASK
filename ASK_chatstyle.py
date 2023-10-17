110a84731e78a7fd7397af7a1895660facd6b75c
Drew W <20048187+dvvilkins@users.noreply.github.com>
2023-10-17 00:32:17 -0400

    Update ASK_chatstyle.py

--- ASK_chatstyle.py
+++ ASK_chatstyle.py
@@ -13,18 +13,14 @@ from ASK_inference import config
 
 st.set_page_config(page_title="ASK Auxiliary Source of Knowledge")
 
-st.markdown(
-    """
-    <style>
-    .css-1jc7ptx, .e1ewe7hr3, .viewerBadge_container__1QSob,
-    .styles_viewerBadge__1yB5_, .viewerBadge_link__1S137,
-    .viewerBadge_text__1JaDK {
-        display: none;
-    }
-    </style>
-    """,
-    unsafe_allow_html=True
-)
+hide_st_style = """
+            <style>
+            #MainMenu {visibility: hidden;}
+            footer {visibility: hidden;}
+            header {visibility: hidden;}
+            </style>
+            """
+st.markdown(hide_st_style, unsafe_allow_html=True)
 
 api_key=st.secrets.QDRANT_API_KEY
 # Check if 'client' is not in locals() or 'client' is not in globals()
