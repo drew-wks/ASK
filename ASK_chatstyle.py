44895d3b1106cee7ccb4a44f24a252285aee1c2d
Drew W <20048187+dvvilkins@users.noreply.github.com>
2023-10-18 19:33:17 -0400

    Update ASK_chatstyle.py
    Removed one approach and trying one from @blackary at streamlit

--- ASK_chatstyle.py
+++ ASK_chatstyle.py
@@ -11,10 +11,6 @@ import ASK_inference as ASK
 
 from ASK_inference import config
 
-from htbuilder import HtmlElement, div, ul, li, br, hr, a, p, img, styles, classes, fonts
-from htbuilder.units import percent, px
-from htbuilder.funcs import rgba, rgb
-
 
 st.set_page_config(page_title="ASK Auxiliary Source of Knowledge")
 
@@ -108,73 +104,25 @@ if query:
 
 
 
-def image(src_as_string, **style):
-    return img(src=src_as_string, style=styles(**style))
+with stylable_container(
+    key="bottom_content",
+    css_styles="""
+        {
+            position: fixed;
+            bottom: 50px;
+        }
+        """,
+):
+    st.markdown("Some content")  # this appears above the chat_input() element.
 
-
-def link(link, text, **style):
-    return a(_href=link, _target="_blank", style=styles(**style))(text)
-
-
-def layout(*args):
-
-    style = """
-    <style>
-      # MainMenu {visibility: hidden;}
-      footer {visibility: hidden;}
-     .stApp { bottom: 105px; }
-    </style>
+st.markdown(
     """
-
-    style_div = styles(
-        position="fixed",
-        left=0,
-        bottom=0,
-        margin=px(0, 0, 0, 0),
-        width=percent(100),
-        color="black",
-        text_align="center",
-        height="auto",
-        opacity=1
-    )
-
-    style_hr = styles(
-        display="block",
-        margin=px(8, 8, "auto", "auto"),
-        border_style="inset",
-        border_width=px(2)
-    )
-
-    body = p()
-    foot = div(
-        style=style_div
-    )(
-        hr(
-            style=style_hr
-        ),
-        body
-    )
-
-    st.markdown(style, unsafe_allow_html=True)
-
-    for arg in args:
-        if isinstance(arg, str):
-            body(arg)
-
-        elif isinstance(arg, HtmlElement):
-            body(arg)
-
-    st.markdown(str(foot), unsafe_allow_html=True)
-
-
-def footer():
-    myargs = [
-        "Made in ",
-        image('https://avatars3.githubusercontent.com/u/45109972?s=400&v=4',
-              width=px(25), height=px(25)),
-        " with ❤️ by ",
-        link("https://twitter.com/ChristianKlose3", "@ChristianKlose3"),
-        br(),
-        link("https://buymeacoffee.com/chrischross", image('https://i.imgur.com/thJhzOO.png')),
-    ]
-    layout(*myargs)
+    <style>
+        .stChatFloatingInputContainer {
+            bottom: 20px;
+            background-color: rgba(0, 0, 0, 0)
+        }
+    </style>
+    """,
+    unsafe_allow_html=True,
+)
