a5e2221fab2e993e692dd9f854ce795ed810013d
Drew W <20048187+dvvilkins@users.noreply.github.com>
2023-10-17 20:41:28 -0400

    Update ASK_chatstyle.py

--- ASK_chatstyle.py
+++ ASK_chatstyle.py
@@ -26,7 +26,7 @@ st.markdown("""
         <style>
                .block-container {
                     padding-top: 1rem;
-                    padding-bottom: 5rem;
+                    padding-bottom: 10rem;
                     padding-left: 3rem;
                     padding-right: 3rem;
                 }
