0752511f5d6e6bc66e51760959e34fcec79f57cc
Drew W <20048187+dvvilkins@users.noreply.github.com>
2023-10-20 20:18:45 -0400

    feedback works so bring stuff back to see when it breaks

--- ASK_chatstyle.py
+++ ASK_chatstyle.py
@@ -4,6 +4,51 @@ from trubrics.integrations.streamlit import FeedbackCollector
 import ASK_inference as ASK
 
 from ASK_inference import config
+from streamlit_extras.stylable_container import stylable_container
+import time
+print(f"Finish imports {datetime.datetime.now().strftime('%H:%M:%S')}")
+print(f"Start css and session states {datetime.datetime.now().strftime('%H:%M:%S')}")
+st.set_page_config(page_title="ASK Auxiliary Source of Knowledge")
+
+hide_st_style = """
+            <style>
+            #MainMenu {visibility: hidden;}
+            footer {visibility: hidden;}
+            header {visibility: visible;}
+            </style>
+            """
+st.markdown(hide_st_style, unsafe_allow_html=True)
+
+st.markdown("""
+        <style>
+               .block-container {
+                    padding-top: 1rem;
+                    padding-bottom: 1rem;
+                    padding-left: 3rem;
+                    padding-right: 3rem;
+                }
+        </style>
+        """, unsafe_allow_html=True)
+
+api_key=st.secrets.QDRANT_API_KEY
+
+
+# Check if 'client' is not in locals() or 'client' is not in globals()
+#if not it runs qdrant_check_and_connect()
+#and places the client object into st.session_state
+if 'clientkey' not in st.session_state:
+    st.session_state.clientkey = []
+    client = ASK.qdrant_connect_cloud(api_key)
+    st.session_state.clientkey = client
+    print(st.session_state.clientkey)
+
+
+print(f"Qdrant start {datetime.datetime.now().strftime('%H:%M:%S')}")
+qdrant = ASK.create_langchain_qdrant(st.session_state.clientkey)
+print(f"Qdrant finish {datetime.datetime.now().strftime('%H:%M:%S')}")
+print(f"Retriever start {datetime.datetime.now().strftime('%H:%M:%S')}")
+retriever = ASK.init_retriever_and_generator(qdrant)
+print(f"Retriever finish {datetime.datetime.now().strftime('%H:%M:%S')}")
 
 
 collector = FeedbackCollector(
