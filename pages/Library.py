a2d573d5799abfeeb5f5dd2f9d073a2204631525
Drew Wilkins <20048187+dvvilkins@users.noreply.github.com>
2023-10-26 19:43:48 -0400

    added a downloader

--- pages/Library.py
+++ pages/Library.py
@@ -61,15 +61,21 @@ if back:
 tab1, tab2, tab3 = st.tabs(["Library Overview", "Document List", "Suggest a Doc"])
 
 with tab1:
-   overview = read_markdown_file("docs/library_overview.md")
-   st.markdown(overview, unsafe_allow_html=True)
+    overview = read_markdown_file("docs/library_overview.md")
+    st.markdown(overview, unsafe_allow_html=True)
 
 with tab2:
-   st.markdown("#### Document List")
-   df = get_pickle_file()
-   display_df = df[['source_short']]
-   edited_df = st.data_editor(display_df, use_container_width=True, hide_index=False, disabled=True)
-   # favorite_command = edited_df.loc[edited_df["rating"].idxmax()]["command"]
+    st.markdown("#### Document List")
+    df = get_pickle_file()
+    display_df = df[['source_short']]
+    edited_df = st.data_editor(display_df, use_container_width=True, hide_index=False, disabled=True)
+    # favorite_command = edited_df.loc[edited_df["rating"].idxmax()]["command"]
+    #downloader
+    isim= 'ASK_library.csv'
+    indir = edited_df.to_csv(index=False)
+    b64 = base64.b64encode(indir.encode(encoding='ISO-8859-1')).decode(encoding='ISO-8859-1')  
+    linko_final= f'<a href="data:file/csv;base64,{b64}" download={isim}>Download List</a>'
+    st.markdown(linko_final, unsafe_allow_html=True)
 
 
 with tab3:
