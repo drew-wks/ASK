6aa905a65417b48e285452115ca46f9f99805f51
Drew Wilkins <20048187+dvvilkins@users.noreply.github.com>
2023-10-26 17:38:11 -0400

    f

--- pages/Library.py
+++ pages/Library.py
@@ -60,7 +60,7 @@ with tab2:
    edited_df = st.data_editor(df)
 
    favorite_command = edited_df.loc[edited_df["rating"].idxmax()]["command"]
-   st.markdown(f"Your favorite command is **{favorite_command}** 🎈")
+
 
 with tab3:
    st.markdown("#### Document Upload")
