6659bac447b71c8a0cac6df82ab7bc3038e57a22
Drew Wilkins <20048187+dvvilkins@users.noreply.github.com>
2023-10-26 15:29:08 -0400

    would single quotes matter?

--- ASK_chatstyle.py
+++ ASK_chatstyle.py
@@ -89,7 +89,7 @@ from streamlit_extras.switch_page_button import switch_page
 
 want_to_contribute = st.button("I want to contribute!")
 if want_to_contribute:
-    switch_page("2_Library")
+    switch_page('Library')
 
     
 
