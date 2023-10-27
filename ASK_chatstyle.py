326419b98ddc1aea70bef3f58a55d32c3fe159aa
Drew Wilkins <20048187+dvvilkins@users.noreply.github.com>
2023-10-26 20:45:37 -0400

    removed vertical lines

--- ASK_chatstyle.py
+++ ASK_chatstyle.py
@@ -106,10 +106,6 @@ if query:
         generation=response['result'],
     )
 
-from streamlit_extras.add_vertical_space import add_vertical_space
-add_n_lines = st.slider("Add n vertical lines below this", 1, 20, 5)
-add_vertical_space(add_n_lines)
-st.write("Here is text after the nth line!")
 
 with stylable_container(
     key="bottom_content",
