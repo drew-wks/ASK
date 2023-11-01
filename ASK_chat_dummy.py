c8abaf5f0ec7ceb02eaba140681f90e2a5bbb99a
Drew Wilkins <20048187+dvvilkins@users.noreply.github.com>
2023-11-01 18:20:23 -0400

    Testing ways to work around st.chat_input UI bug
    https://discuss.streamlit.io/t/place-content-beneath-st-chat-input-element/51075/2

--- ASK_chat_dummy.py
+++ ASK_chat_dummy.py
@@ -1,45 +1,51 @@
-
 import streamlit as st
-import ASK_inference as ASK
-import time
-from ASK_inference import config
+from streamlit_extras.stylable_container import stylable_container
 
-api_key=st.secrets.QDRANT_API_KEY
-
-import os
-os.write(1,b'This is a debugger which outputs to the streamlit console.\n') # 
-
-# Check if 'client' is not in locals() or 'client' is not in globals()
-#if not it runs qdrant_check_and_connect()
-#and places the client object into st.session_state
-if 'clientkey' not in st.session_state:
-    st.session_state.clientkey = []
-    client = ASK.qdrant_connect_cloud(api_key)
-    st.session_state.clientkey = client
-    print(st.session_state.clientkey)
+if "messages" not in st.session_state:
+    st.session_state["messages"] = []
 
 
-qdrant = ASK.create_langchain_qdrant(st.session_state.clientkey)
-retriever = ASK.init_retriever_and_generator(qdrant)
+def generate_response(prompt):
+    return f"This is a response to: {prompt}", 0, 0, 0
 
 
-st.image("https://raw.githubusercontent.com/dvvilkins/ASK/main/images/ASK_logotype_color.png?raw=true", use_column_width="always")
-st.write("#### Get answers to USCG Auxiliary questions from authoritative sources.")
-st.write("ASK uses Artificial Intelligence (AI) to search over [250](https://github.com/dvvilkins/ASK/blob/0e975f41f8f072aac2837ac42a9fe11963dc3fb2/docs/library_doc_list.pdf) Coast Guard Auxiliary references to answer your questions.  Please note: ASK is offered on evaluation basis and has not been officially adopted by the USCG Auxiliary. For questions or feedback, contact [Drew Wilkins](mailto:uscgaux.drew@wks.us).", unsafe_allow_html=True)
+for message in st.session_state["messages"][1:]:
+    with st.chat_message(message["role"]):
+        st.markdown(message["content"])
 
-st.markdown('<a href="2_Library.py" target="_self">linke</a>', unsafe_allow_html=True)
+if prompt := st.chat_input("Type your message..."):
+    st.session_state["messages"].append({"role": "user", "content": prompt})
+    st.chat_message("user").write(prompt)
 
-#response = {}
-query = st.chat_input("Type your question or task here", max_chars=200)
-if query:
-    response = ASK.rag_dummy(query,retriever) # ADDING A DUMMY FUNCTION FOR UNIT TESTING
-    short_source_list = ASK.create_short_source_list(response)
-    long_source_list = ASK.create_long_source_list(response)
-    st.info(f"""##### Response:\n{response['result']}\n\n **Sources:**  \n {short_source_list}\n**Note:**  \nASK may contain inaccuracies. Please review the official documents. Also, ASK only searches natonal documents. Check with your district, division and flotilla leadership for official policy in your AOR.
-    """)
+    with st.spinner("Thinking..."):
+        (
+            full_response,
+            total_tokens,
+            prompt_tokens,
+            completion_tokens,
+        ) = generate_response(prompt)
 
-###############
-    with st.status("Compiling references...", expanded=False) as status:
-        time.sleep(1)
-        st.write(long_source_list)
-        status.update(label=":blue[**Click for references**]", expanded=False)
+    st.chat_message("assistant").write(full_response)
+    st.session_state["messages"].append({"role": "assistant", "content": full_response})
+
+with stylable_container(
+    key="bottom_content",
+    css_styles="""
+        {
+            position: fixed;
+            bottom: 50px;
+        }
+        """,
+):
+    st.markdown("Some content")  # this appears above the chat_input() element.
+
+st.markdown(
+    """
+    <style>
+        .stChatFloatingInputContainer {
+            bottom: 20px;
+            background-color: rgba(0, 0, 0, 0)
+        }
+    </style>
+    """,
+    unsafe_allow_html=True,
