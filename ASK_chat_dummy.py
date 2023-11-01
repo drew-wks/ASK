9c15bb778151daf28a0aa4023e99f28944406b94
Drew Wilkins <20048187+dvvilkins@users.noreply.github.com>
2023-11-01 16:34:35 -0400

    Moving Test into Main remember to remove ask.dummy from the chatstyle.py (#5)
    * Add files via upload
    
    * Create hi
    
    * Add files via upload
    
    * Delete dummy_response.pkl
    
    * Delete test/hi
    
    * Added rag_dummy function
    
    * Updated file with FC response
    
    * Update ASK_chatstyle.py
    
    * added import pickle statement
    
    * added pickle
    
    * turns out pickle is part of python 3.9 so not needed in req file
    
    * fixed comment error
    
    * now looks in test directory for pickle file
    
    * bottom 100px
    
    * dindent
    
    * indent
    
    * added a container
    
    * 100 for style
    
    * 100 for style, 0 for container
    
    * 100 for style and 100 for container
    
    * style 0 , container 200
    
    * 0 style, 100 container, 2 rows text
    
    * 100 style, 50 cont, 2 captions
    
    * remove captions
    
    * removed captions
    
    * notice
    
    * Update ASK_chatstyle.py
    
    * Update ASK_chatstyle.py
    
    * Update ASK_chatstyle.py
    
    * copy tweaks
    
    * Update ASK_chatstyle.py
    
    * Update ASK_chatstyle.py
    
    * simple UI for testing streamlit
    
    * simple python script
    
    * removed more content
    
    * import from relative directory
    
    * added sys.path.append("..")
    
    * back to normal
    
    * moved file out of test/test to test because import didnt work
    
    * removed stragging code from full version
    
    * added in import time
    
    * tessting out streamlit pages
    
    * moving inference.py out of pages
    
    * paying around with pages
    
    * removed.py
    
    * remopved the/
    
    * adding py back on. Becoming very frustrated with slow process introduced gy github and streamlit together. Makes debugging slow
    
    * trying using switch_page
    
    * removed switch_pages
    
    * testing switch_page rom inside pages
    
    * f
    
    * f
    
    * moving everything by this test librabry file outside of pages
    
    * would single quotes matter?
    
    * now that switch_page is working, populate librabry.py file withsome content
    
    * more content. Nothing is displaying on library
    
    * Text works. now testing the return button
    
    * return to app works
    
    * hide sidebar attempt
    
    * f
    
    * implemented hide sidebar on librarby page as well
    
    * added tabs to hold some containers
    
    * content updates
    
    * f
    
    * uploaded a library overview doc as md
    
    * from pathlib import Path
    
    * added the function definition
    
    * updatedoc contents
    
    * more editing
    
    * more updates
    
    * more edits
    
    * indents
    
    * more edits
    
    * more edits
    
    * edtis
    
    * edits
    
    * edits
    
    * edits
    
    * dropping in a sample data frame
    
    * edits
    
    * code indents
    
    * f
    
    * f
    
    * third page placeholder
    
    * f
    
    * f
    
    * f
    
    * f
    
    * f
    
    * f
    
    * f
    
    * f
    
    * added reports folder and a pkl report
    
    * added a pickle file
    
    * add import os
    
    * editing is disabled
    
    * fixed import code
    
    * file path error
    
    * only show short_source
    
    * f
    
    * use container width for dataframe
    
    * enable index
    
    * added a downloader
    
    * import base64
    
    * f
    
    * file uploader
    
    * removed file uploader
    
    * f
    
    * page switch with markdown?
    
    * working, so now implementing
    
    * f
    
    * final push
    
    * changed mailto now that its markdown
    
    * f
    
    * f
    
    * test vertical
    
    * f
    
    * fixed indent
    
    * f
    
    * removed vertical lines
    
    * testing double quotes on switch function
    
    * f
    
    * double quotes works too for switch
    
    * Update ASK_chat_dummy.py

---
+++ ASK_chat_dummy.py
@@ -0,0 +1,45 @@
+
+import streamlit as st
+import ASK_inference as ASK
+import time
+from ASK_inference import config
+
+api_key=st.secrets.QDRANT_API_KEY
+
+import os
+os.write(1,b'This is a debugger which outputs to the streamlit console.\n') # 
+
+# Check if 'client' is not in locals() or 'client' is not in globals()
+#if not it runs qdrant_check_and_connect()
+#and places the client object into st.session_state
+if 'clientkey' not in st.session_state:
+    st.session_state.clientkey = []
+    client = ASK.qdrant_connect_cloud(api_key)
+    st.session_state.clientkey = client
+    print(st.session_state.clientkey)
+
+
+qdrant = ASK.create_langchain_qdrant(st.session_state.clientkey)
+retriever = ASK.init_retriever_and_generator(qdrant)
+
+
+st.image("https://raw.githubusercontent.com/dvvilkins/ASK/main/images/ASK_logotype_color.png?raw=true", use_column_width="always")
+st.write("#### Get answers to USCG Auxiliary questions from authoritative sources.")
+st.write("ASK uses Artificial Intelligence (AI) to search over [250](https://github.com/dvvilkins/ASK/blob/0e975f41f8f072aac2837ac42a9fe11963dc3fb2/docs/library_doc_list.pdf) Coast Guard Auxiliary references to answer your questions.  Please note: ASK is offered on evaluation basis and has not been officially adopted by the USCG Auxiliary. For questions or feedback, contact [Drew Wilkins](mailto:uscgaux.drew@wks.us).", unsafe_allow_html=True)
+
+st.markdown('<a href="2_Library.py" target="_self">linke</a>', unsafe_allow_html=True)
+
+#response = {}
+query = st.chat_input("Type your question or task here", max_chars=200)
+if query:
+    response = ASK.rag_dummy(query,retriever) # ADDING A DUMMY FUNCTION FOR UNIT TESTING
+    short_source_list = ASK.create_short_source_list(response)
+    long_source_list = ASK.create_long_source_list(response)
+    st.info(f"""##### Response:\n{response['result']}\n\n **Sources:**  \n {short_source_list}\n**Note:**  \nASK may contain inaccuracies. Please review the official documents. Also, ASK only searches natonal documents. Check with your district, division and flotilla leadership for official policy in your AOR.
+    """)
+
+###############
+    with st.status("Compiling references...", expanded=False) as status:
+        time.sleep(1)
+        st.write(long_source_list)
+        status.update(label=":blue[**Click for references**]", expanded=False)
---
+++ docs/library_overview.md
@@ -0,0 +1,34 @@
+
+ #### Library Overview
+ ASK has already been loaded with over 250 documents (over 8000 pages) to demonstrate its viability. All these documents are located in public sections of the USCG and USCG Auxiliary websites (cgaux.org uscg.mil). No secure content is included (i.e., content requiring Member Zone or CAC access.
+
+#### Document Types
+
+1. **USCG Auxiliary Directives,** including: 
+   - Auxiliary Manual
+   - Auxiliary Process Guides
+   - Auxiliary Training Handbooks
+   - Auxiliary Programs 
+   - Model Standing Rules  
+
+2. **Other USCG Directives,** such as:
+   - Civil Rights Manual
+   - Navigation Rules
+   - Incident Management Handbook
+   - Contingency Preparedness Planning Manuals
+   - Marine Safety Manuals
+   - Requirements for Uninspected Passenger Vessels  
+
+3. **CHDIRAUX documents** on WOW https://wow.uscgaux.info/content.php?unit=BX-GROUP   
+   - BSX Policy Letters
+   - SOPs
+   - Guides and Handbooks
+   - ALAUXs  
+
+4. **Auxiliary National Documents**
+   - National Leadership Documents located on National Commodore's WOW page
+   - National Directorate Manuals, Handbooks FAQs and Workshops 
+
+5. **Misc. documents:**
+   - Acronym Glossary
+   - New Member Handbook (2015)  
