4b079afa60ff5a2fe21db6327f2433ef9b69823b
Drew Wilkins <20048187+dvvilkins@users.noreply.github.com>
2023-11-02 12:00:56 -0400

    fs

--- test/hello.py
+++ test/hello.py
@@ -1,16 +1,12 @@
 import streamlit as st
 import uuid
-from streamlit_extras.stylable_container import stylable_container
 
 
-
-# Generate a random UUID
 unique_key = str(uuid.uuid4())
 
-user_feedback = " "
+query = " "
 query = st.text_input("Type your question or task here", max_chars=200, key=unique_key)
 
 if query:
-    # Display the input text below the text input widget
-    st.write(f"You typed: {user_input}")
+    st.write(f"You typed: {query}")
 
