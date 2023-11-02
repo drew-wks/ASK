dfe9a347a3c543fb93a6e27d08a47eb551074e80
Drew Wilkins <20048187+dvvilkins@users.noreply.github.com>
2023-11-02 12:09:04 -0400

    test

--- test/hello.py
+++ test/hello.py
@@ -2,17 +2,13 @@ import streamlit as st
 import uuid
 
 
-unique_key = str(uuid.uuid4())
+unique_key = "fiya"
 
 
 query = st.text_input("Type your question or task here", max_chars=200, key=unique_key)
 
 if query:
     st.write(f"You typed: {query}")
+    unique_key = str(uuid.uuid4())
 
-
-haha = st.text_input("Type your question or task here", max_chars=200)
-
-if haha:
-    st.write(f"You typed: {haha}")
 
