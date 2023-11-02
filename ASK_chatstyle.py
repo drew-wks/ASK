55cce1c1592116e176d058effcd5958cb26c92bd
Drew Wilkins <20048187+dvvilkins@users.noreply.github.com>
2023-11-02 11:45:31 -0400

    added a unique key for st.input_text that is randomly generated

--- ASK_chatstyle.py
+++ ASK_chatstyle.py
@@ -7,6 +7,7 @@ import ASK_inference as ASK
 from ASK_inference import config
 from streamlit_extras.stylable_container import stylable_container
 import time
+import uuid
 st.set_page_config(page_title="ASK Auxiliary Source of Knowledge", initial_sidebar_state="collapsed")
 
 
@@ -73,8 +74,11 @@ examples.write("""
 """)
 st.write("  ")
 
+# Generate a random UUID
+unique_key = str(uuid.uuid4())
+
 user_feedback = " "
-query = st.text_input("Type your question or task here", max_chars=200)
+query = st.text_input("Type your question or task here", max_chars=200, key=unique_key)
 if query:
     with st.status("Checking documents...", expanded=False) as status:
         if query == "pledge":
