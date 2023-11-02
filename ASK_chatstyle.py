40dd1e7f68040da077409f9e0ab8a154ba2592c6
Drew Wilkins <20048187+dvvilkins@users.noreply.github.com>
2023-11-01 20:20:52 -0400

    placing in a form erase. Not sure this will work

--- ASK_chatstyle.py
+++ ASK_chatstyle.py
@@ -75,9 +75,18 @@ st.write("  ")
 st.write("  ")
 
 user_feedback = " "
-query = st.empty
-query = st.text_input("Type your question or task here", max_chars=200)
-if query:
+
+if "my_text" not in st.session_state:
+    st.session_state.my_text = ""
+
+def submit():
+    st.session_state.my_text = st.session_state.widget
+    st.session_state.widget = ""
+
+
+st.text_input("Type your question or task here", max_chars=200, key="widget", on_change=submit)
+my_text = st.session_state.my_text
+if my_text:
     with st.status("Checking documents...", expanded=False) as status:
         if query == "pledge":
             response = ASK.rag_dummy(query,retriever) # ASK.rag_dummy for UNIT TESTING
@@ -86,7 +95,7 @@ if query:
         short_source_list = ASK.create_short_source_list(response)
         long_source_list = ASK.create_long_source_list(response)
         examples.empty()
-        st.info(f"""**Query:** *{query}*\n\n ##### Response:\n{response['result']}\n\n **Sources:**  \n {short_source_list}\n**Note:**  \nASK can make mistakes. Verify with the sources. Also, ASK is a national service. Check with your AOR for additional policies.
+        st.info(f"""*{query}*\n\n ##### Response:\n{response['result']}\n\n **Sources:**  \n {short_source_list}\n**Note:**  \nASK can make mistakes. Verify with the sources. Also, ASK is a national service. Check with your AOR for additional policies.
         """)
     status.update(label=":blue[**Response**]", expanded=True)
 
