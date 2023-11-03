551f79a519dfec06c0dce22a420489c5cc7cd871
Drew Wilkins <20048187+dvvilkins@users.noreply.github.com>
2023-11-03 00:13:44 -0400

    copy tweak

--- ASK_chatstyle.py
+++ ASK_chatstyle.py
@@ -60,7 +60,7 @@ st.image("https://raw.githubusercontent.com/dvvilkins/ASK/main/images/ASK_logoty
 
 st.write(
     "#### Get answers to USCG Auxiliary questions from authoritative sources.")
-st.markdown("ASK uses Artificial Intelligence (AI) to search over 250 Coast Guard Auxiliary references for answers. ASK was created by Drew Wilkins for evaluation. Not officially adopted by the USCG Auxiliary. Learn more about ASK <a href='Library' target='_self'>here.</a>", unsafe_allow_html=True)
+st.markdown("ASK uses Artificial Intelligence (AI) to search over 250 Coast Guard Auxiliary references for answers. ASK was created by Drew Wilkins for evaluation. Not officially adopted by the USCG Auxiliary. Click <a href='Library' target='_self'>here </a>"to leean more., unsafe_allow_html=True)
 
 examples = st.empty()
 
