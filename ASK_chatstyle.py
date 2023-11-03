67a9129ae55c0d6fd68bdb884a035f82346af837
Drew Wilkins <20048187+dvvilkins@users.noreply.github.com>
2023-11-03 01:57:19 -0400

    Update ASK_chatstyle.py

--- ASK_chatstyle.py
+++ ASK_chatstyle.py
@@ -87,7 +87,7 @@ if query:
         long_source_list = ASK.create_long_source_list(response)
         examples.empty()
 
-        st.info(f"**Question:** *{query}*\n\n ##### Response:\n{response['result']}\n\n **Sources:**  \n{short_source_list}\n**Note:** \n ASK can make mistakes. Verify the sources and check for local policy.")
+        st.info(f"**Question:** *{query}*\n\n ##### Response:\n{response['result']}\n\n **Sources:**  \n{short_source_list}\n **Note:** \n ASK can make mistakes. Verify the sources and check for local policy.")
     status.update(label=":blue[**Response**]", expanded=True)
 
     with st.status("Compiling references...", expanded=False) as status:
