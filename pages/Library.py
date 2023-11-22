b7c14aca9b5090faf8814390cff2a413a04953f8
Drew Wilkins <20048187+drew-wks@users.noreply.github.com>
2023-11-22 14:08:36 -0500

    fs

--- pages/Library.py
+++ pages/Library.py
@@ -89,8 +89,7 @@ with tab4:
 with tab5:
     st.markdown("#### Feedback")
     st.markdown("ASK's mission is to provide USCG Auxiliary members efficient, accuracete and easy access to the authoritative source of knowledge on any topic in the Auxiliary.")
-    st.write("")
-    st.markdown('If you would like to help ASK acheive this mission, please reach out!')
+    st.markdown('If you would like to help ASK acheive this mission, **please reach out!**')
                 
     st.markdown('''Presently, ASK works by analyzing documents that are the most current official policy that exists at a national level. 
              If you see a document missing from the libary or should be removed, please let us know.''')  
