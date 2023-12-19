0a3fbf7303206dcf49869f5689a2219e7140e618
Drew Wilkins <20048187+drew-wks@users.noreply.github.com>
2023-12-18 21:07:22 -0500

    fs

--- pages/Library.py
+++ pages/Library.py
@@ -73,7 +73,7 @@ with tab2:
     if df is not None:
         num_items = len(df)
         st.markdown("#### Library Overview")
-        st.markdown(f"{overview} There are currently {num_items} items in the ASK Library.  \n")
+        st.markdown(f"{overview} \n There are currently {num_items} items in the ASK Library.")
         #st.markdown(overview, unsafe_allow_html=True)
 
         # Display the DataFrame
