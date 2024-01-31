07d1ef01503eeb072af37f0bc7c74edb303184bc
Drew Wilkins <20048187+drew-wks@users.noreply.github.com>
2024-01-31 15:24:52 -0500

    fs

--- prompt_ui.py
+++ prompt_ui.py
@@ -50,7 +50,7 @@ if "operational" not in api_status_message:
     st.error(f"ASK is currently down due to OpenAI {api_status_message}.")
 else: st.write("#### Get answers to USCG Auxiliary questions from authoritative sources.")
 
-df, last_update_date = utils.get_most_recent_filepath_and_date("library_catalog", "docs/library_catalog/", "xlsx")
+df, last_update_date = utils.get_most_recent_filepath_and_date("library_catalog_", "docs/library_catalog/", "xlsx")
 num_items = len(df)
 
 
