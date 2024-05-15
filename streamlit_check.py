7951d38888abd38f6ea6040cf5d889a6a5fc8896
Drew Wilkins <20048187+drew-wks@users.noreply.github.com>
2024-05-15 15:39:30 -0400

    update the check streamlit code to pass an error to github actions if streamlit isn'tup

--- streamlit_check.py
+++ streamlit_check.py
@@ -7,7 +7,10 @@ def check_streamlit_app(url):
             print("ASK Streamlit app is up and running!")
         else:
             print("ASK Streamlit app is not reachable. Status code:", response.status_code)
+            sys.exit(1)  # Exits with error if status code is not 200
     except requests.exceptions.RequestException as e:
-        print("An error occurred: ", e)
+        print(f"An error occurred: {e}")
+        sys.exit(1)  # Exits with error if there's an exception
+
 
 check_streamlit_app('https://uscg-auxiliary-ask.streamlit.app/')
