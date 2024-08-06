06d7baf896724159a03b06011bd4b0b04518fed9
Drew Wilkins <20048187+drew-wks@users.noreply.github.com>
2024-08-05 20:57:29 -0400

    update various parts of the qdrant check code

--- qdrant_check.py
+++ qdrant_check.py
@@ -1,26 +1,27 @@
 import os
 import traceback
 from qdrant_client import QdrantClient
+import pprint
 
 '''
-Checks the status of the qdrant db. 
+Checks the status of the Qdrant database.
 
 To check if it ran correctly, 
-1. the Requests tab of the Qdrant Cluster Details page should list a request to the /collections/{collection_name} endpoint
-2. The Logs tab shluld have a mesage related to the collection ace such as Fetching collection 'ASK_vectorstore
+1. The Requests tab of the Qdrant Cluster Details page should list a request to the /collections/{collection_name} endpoint.
+2. The Logs tab should have a message related to the collection ace such as "Fetching collection 'ASK_vectorstore'".
 
-
-To add email report of results implement the code here:
+To add an email report of results, implement the code here:
 https://www.youtube.com/watch?v=2OwLb-aaiBQ
 '''
 
+# Fetching environment variables
+url = os.environ.get("QDRANT_URL")
+api_key = os.environ.get("QDRANT_API_KEY")  # Only required for Qdrant Cloud
 
-url=os.environ.get("QDRANT_URL")
-api_key=os.environ.get("QDRANT_API_KEY")  # Only required for Qdrant Cloud
-    
+# Function to connect to Qdrant
 def qdrant_connect_cloud(api_key, url):
     try:
-        print("attempting to assign client")
+        print("Attempting to assign client...")
         client = QdrantClient(
             url=url,
             prefer_grpc=True,
@@ -32,13 +33,22 @@ def qdrant_connect_cloud(api_key, url):
         traceback.print_exc()
         return None
 
-
+# Connect to Qdrant
 client = qdrant_connect_cloud(api_key, url)
+
 if client:
     try:
+        # Fetching collection information
         collection = client.get_collection('ASK_vectorstore')
         print("Collection name:", 'ASK_vectorstore')
         print("Collection status:", collection.status)
+        
+        # Pretty print the collection information
+        pp = pprint.PrettyPrinter(width=80, compact=False)
+        pp.pprint(collection)
+        
     except Exception as e:
-        print("An error occurred while getting collections:")
+        print("An error occurred while getting the collection:")
         print(e)
+else:
+    print("Client is None, unable to proceed.")
