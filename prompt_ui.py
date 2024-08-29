6e09f89cd08787523d390701233076bffdc7ae7b
Drew Wilkins <20048187+drew-wks@users.noreply.github.com>
2024-08-29 10:55:49 -0400

    Update prompt_ui.py thorough comments

--- prompt_ui.py
+++ prompt_ui.py
@@ -1,24 +1,28 @@
 import streamlit as st 
 from trubrics.integrations.streamlit import FeedbackCollector
-import ASK_inference as ASK
+import ASK_inference as ASK # both scripts must be in same directory for this to work
 from ASK_inference import config
 import datetime, time
 from streamlit_extras.stylable_container import stylable_container
 
 
+# Collapse the sidebar and hide the sidebar collapse button
 st.set_page_config(page_title="ASK Auxiliary Source of Knowledge", initial_sidebar_state="collapsed")
-
 st.markdown( """ <style> [data-testid="collapsedControl"] { display: none } </style> """, unsafe_allow_html=True, )
 
-hide_st_style = """
+
+# Hide Streamlit's default UI elements: Main menu, footer, and header
+hide_streamlit_ui = """
             <style>
             #MainMenu {visibility: hidden;}
             footer {visibility: hidden;}
             header {visibility: hidden;}
             </style>
             """
-st.markdown(hide_st_style, unsafe_allow_html=True)
+st.markdown(hide_streamlit_ui, unsafe_allow_html=True)
 
+
+# Adjust the padding around the main content area for a cleaner layout
 st.markdown("""
         <style>
                 .block-container {
@@ -31,17 +35,21 @@ st.markdown("""
         """, unsafe_allow_html=True)
 
 
-qdrant_connect_cloud_cached = st.cache_resource(ASK.qdrant_connect_cloud)
-api_key = st.secrets.QDRANT_API_KEY
-url = st.secrets.QDRANT_URL
-# client = ASK.qdrant_connect_cloud(api_key, url) # use this for ask-test so you can see the changes
-client = qdrant_connect_cloud_cached(api_key, url) # use this version for ask-main for speed
-qdrant = ASK.create_langchain_qdrant(client)
-retriever = ASK.init_retriever_and_generator(qdrant)
+# Initialize the RAG clients and retriver
+try:
+    # Cache the Qdrant client connection for performance optimization
+    qdrant_connect_cloud_cached = st.cache_resource(ASK.qdrant_connect_cloud)
+    api_key = st.secrets.QDRANT_API_KEY
+    url = st.secrets.QDRANT_URL
+    client = qdrant_connect_cloud_cached(api_key, url) # use ASK.qdrant_connect_cloud(api_key, url) instead for ask-test so you can see the changes
+    qdrant = ASK.create_langchain_qdrant(client)
+    retriever = ASK.init_retriever_and_generator(qdrant)
+except Exception as e:
+    st.error(f"An error occurred while setting up the Qdrant client: {e}")
+    st.stop() 
 
 
-# Manage Trubrics authentication token with session state
-# see feedback at https://trubrics.streamlit.app/?ref=blog.streamlit.io
+# Set up the Truberics feedback collector. feedback is accessible at https://trubrics.streamlit.app/?ref=blog.streamlit.io
 @st.cache_data
 def get_feedback_collector():
     return FeedbackCollector(
@@ -54,19 +62,20 @@ def get_feedback_collector():
 st.image("https://raw.githubusercontent.com/dvvilkins/ASK/main/images/ASK_logotype_color.png?raw=true", use_column_width="always")
 
 
+# Check Open AI service status
 api_status_message = ASK.get_openai_api_status()
 if "operational" not in api_status_message:
     st.error(f"ASK is currently down due to OpenAI {api_status_message}.")
 else: st.write("#### Get answers to USCG Auxiliary questions from authoritative sources.")
 
+
+# Get the library catalog
 df, last_update_date = ASK.get_library_doc_catalog_excel_and_date()
 num_items = len(df)
 
 
 st.markdown(f"ASK uses Artificial Intelligence (AI) to search over {num_items} Coast Guard Auxiliary references for answers. This is a working prototype for evaluation. Not an official USCG Auxiliary service. Learn more <a href='Library' target='_self'><b>here</b>.</a>", unsafe_allow_html=True)
-
 examples = st.empty()
-
 examples.write("""  
     **ASK answers questions such as:**   
     *What are the requirements to run for FC?*  
@@ -74,9 +83,10 @@ examples.write("""
     *¿En que ocasiones es necesario un saludo militar?*   
     
 """)
-
 st.write("  ")
 
+
+# Accept a user query and run a RAG pipeline
 user_feedback = " "
 user_question = st.text_input("Type your question or task here", max_chars=200)
 if user_question:
@@ -111,22 +121,23 @@ if user_question:
         st.write(query)
         status.update(label=":blue[CLICK HERE FOR FULL SOURCE DETAILS]", expanded=False)
 
+            
+    # Send the prompt used and any feedback to Trubrics feedback collector
     collector.log_prompt(
         config_model={"model": "gpt-3.5-turbo"},
         prompt=query,
         generation=response['result'],
         )
-    
-
-    user_feedback = collector.st_feedback(
+    collector.st_feedback(
         component="default",
         feedback_type="thumbs",
         open_feedback_label="[Optional] Provide additional feedback",
         model="gpt-3.5-turbo",
-        prompt_id=None,  # checkout collector.log_prompt() to log your user prompts
+        prompt_id=None,
         )
 
 
+# Lock the chat input container 50 pixels above bottom of viewport
 with stylable_container(
     key="bottom_content",
     css_styles="""
